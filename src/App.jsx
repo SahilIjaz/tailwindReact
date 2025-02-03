8b733b019710525e3e2e888cf9e5fa4cd9d1ef54
SahilIjaz <hssahil2913@gmail.com>
2025-02-03 17:49:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Card from './componenets/card'
+import './App.css'
+
+function App(){
+  return (
+    <><h1 className='bg-green-400'>Hello Tailwind</h1>
+    <Card/>
+   
+    </>
+  )
+}
+
+export default App
